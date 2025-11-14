3474a8df43db665b28ceeb79e2b3db7fe96dae49
MaxS2027 <max.schwartz2027@gbwl.org>
2025-11-14 13:54:22 +0000

    2.7 notes

--- Main.java
+++ Main.java
@@ -2,5 +2,21 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+
+      //WHILE loops repeat instructions while some condition is true
+      //as long as the certain condition is TRUE
+
+      //COUntER CONTROLLER while less
+      //step #1 initialize control variable
+      int count = 10;
+      count++;
+      count++;
+      count
+      //step two make a condition
+      while(count<= 5){
+         //loop body executes while count (<= 5) is true
+         System.out.println("1");
+         count++;
+      }
    }
 }
