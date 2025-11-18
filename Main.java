163ca7f0dfed52f1d84ca137fc0ad3af7de8f41f
MaxS2027 <max.schwartz2027@gbwl.org>
2025-11-18 16:06:16 +0000

    2.8 notes

--- Main.java
+++ Main.java
@@ -11,12 +11,17 @@ public class Main {
       int count = 10;
       count++;
       count++;
-      count
       //step two make a condition
       while(count<= 5){
          //loop body executes while count (<= 5) is true
          System.out.println("1");
          count++;
+      }
+      for(int i = 10 ; i<= 27; i +=2){
+         System.out.println("i");
+      }
+      for(l = 10; l<=0; l+=1){
+         System.out.println(l + " ");
       }
    }
 }
